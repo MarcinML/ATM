2f29d0406304e7f1430e185b3bd4fcf88811929d
Marcin Liszcz <marcinliszcz@gmail.com>
2018-07-24 21:53:24 +0200

    Initial commit

---
+++ Component.h
@@ -0,0 +1,26 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+#include "Message.h"
+
+class Component
+{
+public:
+    virtual ~Component() = default;
+
+    virtual void handle(const Message& message) = 0;
+
+    void setNext(const Component& component)
+    {
+        if (next != nullptr)
+        {
+            throw std::exception("Cannot change next");
+        }
+        next = component;
+    }
+protected:
+    Component * next = nullptr;
+
+};
+
+#endif //COMPONENT_H
